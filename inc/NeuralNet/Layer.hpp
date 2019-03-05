9d87e44b270471615ec8ef6f0a9cbf92888bf4e3
Dorian'B <dorian.bachelot@hotmail.com>
2019-03-05 10:42:43 +0100

    [NeuralNet] Added a new module : NeuralNet.

---
+++ inc/NeuralNet/Layer.hpp
@@ -0,0 +1,44 @@
+/**
+*
+*   TIGINET
+*   Copyright (C) 2018  BACHELOT Dorian
+*
+*   For contact see CONTACT.md file.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*	See LICENSE file for more.
+*
+*/
+
+#ifndef TN_NEURALNET_LAYER_HPP
+#define TN_NEURALNET_LAYER_HPP
+
+#include <NeuralNet/Config.hpp>
+
+namespace TN
+{
+
+	template<typename T>
+	class TN_NEURALNET Layer
+	{
+	public:
+		virtual void Propagate() = 0;
+		virtual void Learn() = 0;
+		
+	};
+
+}
+
+#endif
