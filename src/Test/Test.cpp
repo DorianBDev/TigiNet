b14d4ae6165209c857b27ff21cea51769053e3e6
DorianBDev <dorian.bachelot@hotmail.com>
2019-03-13 20:41:33 +0100

    [Test] Updated the log system to a C++ one.

--- src/Test/Test.cpp
+++ src/Test/Test.cpp
@@ -23,6 +23,7 @@
 */
 
 #include <Core/Log.hpp>
+#include <Core/Error.hpp>
 #include <NeuralNet/Layer.hpp>
 #include <Utility/Tensor.hpp>
 #include <Utility/Gradient.hpp>
@@ -32,9 +33,9 @@ int main()
 {
 	TN::ActivatorConfig<double> config(TN::Sigmoide);
 	double res = config.ActivationFunction(0);
-	TN_LOG("%f", res);
+	TN_LOG("TEST") << res;
 	res = config.ActivationDerivative(0.5);
-	TN_LOG("%f", res);
+	TN_LOG("TEST") << res;
 
 	TN::gradient_t<double> grad;
 	grad.actual = 10;
@@ -42,16 +43,16 @@ int main()
 	TN::ActivatorConfig<int> config2(TN::Sigmoide);
 	TN::FCLayer<int> l(config2, 2);
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
 
 	TN::Tensor<int> s(0, TN::TensorShape(NULL), 2);
 
 	s() = 0;
 
-	TN_LOG("%d", s());
+	TN_LOG("TEST") << s();
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
 	unsigned int vshape[] = { 2 };
 	TN::Tensor<int> v(1, TN::TensorShape(vshape), 0);
@@ -59,9 +60,9 @@ int main()
 	v(0) = 0;
 	v(1) = 1;
 
-	TN_LOG("%d %d", v(0), v(1));
+	TN_LOG("TEST") << v(0) << " " << v(1);
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
 	unsigned int shape[] = { 2, 2, 2 };
 	TN::Tensor<int> t(3, TN::TensorShape(shape), 3);
@@ -77,28 +78,28 @@ int main()
 	t[1][1](0) = 7;
 	t[1][1](1) = 8;
 
-	TN_LOG("0 : %d %d", t[0][0](0), t[0][0](1));
-	TN_LOG("0 : %d %d", t[0][1](0), t[0][1](1));
-	TN_LOG("1 : %d %d", t[1][0](0), t[1][0](1));
-	TN_LOG("1 : %d %d", t[1][1](0), t[1][1](1));
+	TN_LOG("TEST") << "0 : " << t[0][0](0) << " " << t[0][0](1);
+	TN_LOG("TEST") << "0 : " << t[0][1](0) << " " << t[0][1](1);
+	TN_LOG("TEST") << "1 : " << t[1][0](0) << " " << t[1][0](1);
+	TN_LOG("TEST") << "1 : " << t[1][1](0) << " " << t[1][1](1);
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
 	int* data = t.GetData();
-	TN_LOG("0 : %d %d", data[0], data[1]);
-	TN_LOG("0 : %d %d", data[2], data[3]);
-	TN_LOG("1 : %d %d", data[4], data[5]);
-	TN_LOG("1 : %d %d", data[6], data[7]);
+	TN_LOG("TEST") << "0 : " << data[0] << " " << data[1];
+	TN_LOG("TEST") << "0 : " << data[2] << " " << data[3];
+	TN_LOG("TEST") << "1 : " << data[4] << " " << data[5];
+	TN_LOG("TEST") << "1 : " << data[6] << " " << data[7];
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
 	data = t[1].GetData();
-	TN_LOG("1 : %d %d", data[0], data[1]);
-	TN_LOG("1 : %d %d", data[2], data[3]);
+	TN_LOG("TEST") << "1 : " << data[0] << " " << data[1];
+	TN_LOG("TEST") << "1 : " << data[2] << " " << data[3];
 
-	TN_LOG("-------");
+	TN_LOG("TEST") << "-------";
 
-	TN_LOG("End")
+	TN_LOG("TEST") << "END";
 
 	return 0;
 }
