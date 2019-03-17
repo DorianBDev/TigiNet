f8c1016cba7a1c03bdca440ba9a79a71812b2152
DorianBDev <dorian.bachelot@hotmail.com>
2019-03-17 18:50:46 +0100

    [Test] Tested FCLayer propagation.

--- src/Test/Test.cpp
+++ src/Test/Test.cpp
@@ -33,37 +33,8 @@
 
 int main()
 {
-	TN::ActivatorConfig<double> config(TN::Sigmoide);
-	double res = config.ActivationFunction(0);
-	TN_LOG("TEST") << res;
-	res = config.ActivationDerivative(0.5);
-	TN_LOG("TEST") << res;
 
-	TN::gradient_t<double> grad;
-	grad.actual = 10;
-
-	TN::FCLayer<double> l(TN::ActivatorConfig<double>(TN::Sigmoide), TN::UniformInitializer<double>(), 2);
-
-	TN_LOG("TEST") << "-------";
-
-
-	TN::Tensor<int> s(0, TN::TensorShape(NULL), 2);
-
-	s() = 0;
-
-	TN_LOG("TEST") << s();
-
-	TN_LOG("TEST") << "-------";
-
-	unsigned int vshape[] = { 2 };
-	TN::Tensor<int> v(1, TN::TensorShape(vshape), 0);
-
-	v(0) = 0;
-	v(1) = 1;
-
-	TN_LOG("TEST") << v(0) << " " << v(1);
-
-	TN_LOG("TEST") << "-------";
+	TN::FCLayer<double> l(TN::ActivatorConfig<double>(TN::Sigmoide), TN::UniformInitializer<double>(), 3);
 
 	unsigned int shape[] = { 2, 2, 2 };
 	TN::Tensor<double> t(3, TN::TensorShape(shape), 3);
@@ -81,63 +52,11 @@ int main()
 
 	t.Print();
 
-	TN_LOG("TEST") << "0 : " << t[0][0](0) << " " << t[0][0](1);
-	TN_LOG("TEST") << "0 : " << t[0][1](0) << " " << t[0][1](1);
-	TN_LOG("TEST") << "1 : " << t[1][0](0) << " " << t[1][0](1);
-	TN_LOG("TEST") << "1 : " << t[1][1](0) << " " << t[1][1](1);
+	l.Activate();
 
 	TN_LOG("TEST") << "-------";
 
-	double* data = t.GetData();
-	TN_LOG("TEST") << "0 : " << data[0] << " " << data[1];
-	TN_LOG("TEST") << "0 : " << data[2] << " " << data[3];
-	TN_LOG("TEST") << "1 : " << data[4] << " " << data[5];
-	TN_LOG("TEST") << "1 : " << data[6] << " " << data[7];
-
-	TN_LOG("TEST") << "-------";
-
-	data = t[1].GetData();
-	TN_LOG("TEST") << "1 : " << data[0] << " " << data[1];
-	TN_LOG("TEST") << "1 : " << data[2] << " " << data[3];
-
-	TN_LOG("TEST") << "-------";
-
-	TN::ZeroInitializer<double> z;
-
-	z.Initialize(t);
-	t.Print();
-
-	TN::UniformInitializer<double> y;
-	y.Initialize(t);
-	t.Print();
-
-	TN_LOG("TEST") << "-------";
-
-	unsigned int shape2[] = { 2, 2 };
-
-	TN::Tensor<int> t1(2, TN::TensorShape(shape2), 2);
-	t1[0](0) = 1;
-	t1[0](1) = 2;
-	t1[1](0) = 3;
-	t1[1](1) = 4;
-	t1.Print();
-
-	TN_LOG("TEST") << "-------";
-
-	TN::Tensor<int> t2(2, TN::TensorShape(shape2), 2);
-	t2[0](0) = 1;
-	t2[0](1) = 2;
-	t2[1](0) = 3;
-	t2[1](1) = 4;
-	t2.Print();
-
-	TN_LOG("TEST") << "-------";
-
-	TN::Tensor<int> * t3 = MatrixProduct(t1, t2);
-
-	t3->Print();
-
-	delete t3;
+	l.GetOutput().Print();
 
 	TN_LOG("TEST") << "-------";
 
