4fd58ba141fba104771d80bcc12bf19c86e14124
DorianBDev <dorian.bachelot@hotmail.com>
2019-03-19 20:20:03 +0100

    [NeuralNet] Added a virtual class to keep a cost function and his derivative.

---
+++ inc/NeuralNet/Cost.hpp
@@ -0,0 +1,127 @@
+/**
+*
+*   TIGINET
+*   Copyright (C) 2018-2019  BACHELOT Dorian
+*
+*   For contact see CONTACT.md file.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*	See LICENSE file for more.
+*
+*/
+
+#ifndef TN_NEURALNET_COST_HPP
+#define TN_NEURALNET_COST_HPP
+
+#include <Utility/Tensor.hpp>
+
+namespace TN
+{
+	/**
+	* @class CostFunction
+	* @brief This virtual class hold a cost function and its derivative.
+	*
+	* A pure virtual class that handle a cost function and its derivative.
+	*
+	*/
+	template<typename T>
+	class CostFunction
+	{
+	public:
+
+		/**
+		* @brief Calculate error.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the error.
+		*
+		*/
+		virtual T Calculate(const T& prediction, const T& result) const = 0;
+
+		/**
+		* @brief Calculate error from tensors.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the error.
+		*
+		*/
+		virtual T Calculate(const Tensor<T>& prediction, const Tensor<T>& result) const = 0;
+
+		/**
+		* @brief Measures the importance of a prediction in the error.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the measurement.
+		*
+		*/
+		virtual T Derivative(const T& prediction, const T& result) = 0;
+	};
+
+	/**
+	* @class MeanSquaredError
+	* @brief MSE cost function class.
+	*
+	* MSE : 1/2(sum(pow(prediction - result))
+	*
+	*/
+	template<typename T>
+	class MeanSquaredError : public CostFunction<T>
+	{
+	public:
+
+		/**
+		* @brief Calculate error.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the error.
+		*
+		*/
+		T Calculate(const T& prediction, const T& result) const;
+
+		/**
+		* @brief Calculate error from tensors.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the error.
+		*
+		*/
+		T Calculate(const Tensor<T>& prediction, const Tensor<T>& result) const;
+
+		/**
+		* @brief Measures the importance of a prediction in the error.
+		*
+		* @param prediction : your prediction.
+		* @param result : your expected result.
+		*
+		* @return Return the measurement.
+		*
+		*/
+		T Derivative(const T& prediction, const T& result) const;
+	};
+}
+
+#include <NeuralNet/Cost.inl>
+
+#endif
---
+++ inc/NeuralNet/Cost.inl
@@ -0,0 +1,70 @@
+/**
+*
+*   TIGINET
+*   Copyright (C) 2018-2019  BACHELOT Dorian
+*
+*   For contact see CONTACT.md file.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*	See LICENSE file for more.
+*
+*/
+
+#ifndef TN_NEURALNET_COST_INL
+#define TN_NEURALNET_COST_INL
+
+namespace TN
+{
+	/// @private
+	template<typename T>
+	T MeanSquaredError<T>::Calculate(const T& prediction, const T& result) const
+	{
+		return static_cast<T>(static_cast<double>(1) / static_cast<double>(2) * (prediction - result) * (prediction - result));
+	}
+
+	/// @private
+	template<typename T>
+	T MeanSquaredError<T>::Calculate(const Tensor<T>& prediction, const Tensor<T>& result) const
+	{
+		TN_ASSERT(prediction.GetRank() == result.GetRank(), "NEURALNET", "Wrong tensors rank");
+
+		if(prediction.GetRank() == 0)
+			return Calculate(prediction(), result());
+
+		T res = 0;
+
+		if (prediction.GetRank() == 1)
+		{
+			for (unsigned int i = 0; i < prediction.GetDimension(1); i++)
+				res += Calculate(prediction(i), result(i));
+
+			return res;
+		}
+
+		for (unsigned int i = 0; i < prediction.GetDimension(prediction.GetRank()); i++)
+			res += Calculate(prediction[i], result[i]);
+
+		return res;
+	}
+
+	/// @private
+	template<typename T>
+	T MeanSquaredError<T>::Derivative(const T& prediction, const T& result) const
+	{
+		return prediction - result;
+	}
+}
+
+#endif
